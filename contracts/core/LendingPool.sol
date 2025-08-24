5c6eaf0e039661f696549703237868b75351c7ac
Sangharsha <upadhyayasangharsha@gmail.com>
2025-08-24 09:38:05 +0545

    Add: Admins

--- contracts/core/LendingPool.sol
+++ contracts/core/LendingPool.sol
@@ -12,14 +12,16 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract LendingPollV1 is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    uint256 private constant BPS = 10_000;
+    uint256 private constant BPS = 10_000; // 100% BPS
     uint256 private constant WAD = 1e18;
 
     // admin's 
     address public owner;
     AssetConfig public assetConfig;
     PriceOracle public priceOracle;
-
+    // owner: The god-mode address. Can list assets, change parameters.
+    // assetConfig: External contract for all asset risk configs (keeps pool lean and modular).
+    // priceOracle: External contract for price feeds (no on-chain price magic here).
 
     mapping(address => mapping(address => uint256)) public borrowBalances;
     address[] public listedAssets;
@@ -39,9 +41,29 @@ contract LendingPollV1 is ReentrancyGuard {
         uint256 repayAmount,
         uint256 seizedAmount);
 
-    event ();
-    event ();
-    event ();
+    event OwnerUpdated(address indexed newOner);
+    event AssestListed(address indexed asset);
+    event closeFactorUpdated(uint256 newClosedFactorBps);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner , "Not the owner");
+        _;
+    }
+
+    constructor(address _assetConfig, address _priceOracle){
+        require(_assetConfig != address(0) && _priceOracle != address(0), "bad deps");
+        owner = msg.sender;
+        assetConfig=AssetConfig(_assetConfig);
+        priceOracle=PriceOracle(_priceOracle);
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner{
+        require(newOwner != address(0),"zero");
+        owner= newOwner;
+        emit OwnerUpdated(newOwner);
+    }
+
+
 
 
 
